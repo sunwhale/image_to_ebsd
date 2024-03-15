aaaeeff9cfeabcafa40946fb53f0aeca1ddc929e
sunwhale <sunwhale@126.com>
2024-03-15 11:55:42 +0800

    Update create_ebsd.py

--- create_ebsd.py
+++ create_ebsd.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import numpy as np
 import os
+import time
+from scipy.interpolate import RegularGridInterpolator
 
 
 def create_ctf():
@@ -33,6 +35,11 @@ Phase	X	Y	Bands	Error	Euler1	Euler2	Euler3	MAD	BC	BS
     f.close()
 
 
+def find_closest_vector(target_vector, vectors):
+    a = (target_vector - vectors[:, -3:]).astype('int')
+    return vectors[np.argmin(np.sum(a ** 2, axis=1))]
+
+
 def read_euler():
     image = Image.open('euler.bmp')
     image_array = np.array(image)
@@ -49,11 +56,35 @@ def read_euler():
     unique_rows, indices = np.unique(euler_to_color[:, -3:], axis=0, return_index=True)
     euler_to_color = euler_to_color[indices]
 
-    print(euler_to_color.shape)
+    print(euler_to_color)
+
+    r = np.array([i for i in range(0, 256, 1)], dtype=int)
+    g = np.array([i for i in range(0, 256, 1)], dtype=int)
+    b = np.array([i for i in range(0, 256, 1)], dtype=int)
+
+    euler2 = np.zeros((len(r), len(g), len(b)))
+    euler3 = np.zeros((len(r), len(g), len(b)))
+
+    for i in range(len(r)):
+        for j in range(len(g)):
+            time_0 = time.time()
+            for k in range(len(b)):
+                euler_angles = find_closest_vector(np.array([r[i], g[j], b[k]]), euler_to_color)
+                euler2[i, j, k] = euler_angles[1]
+                euler3[i, j, k] = euler_angles[2]
+            time_1 = time.time()
+            print(i, j, time_1 - time_0)
+
+    interpolator = RegularGridInterpolator((r, g, b), euler2)
 
     np.save('euler_to_color.npy', euler_to_color)
+    np.save('r.npy', r)
+    np.save('g.npy', g)
+    np.save('b.npy', b)
+    np.save('euler2.npy', euler2)
+    np.save('euler3.npy', euler3)
 
-    print(euler_to_color)
+    # print(euler_to_color)
 
 
 if __name__ == '__main__':
